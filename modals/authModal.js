d1ab319507d76edc1ac516f5e5f698426e016d1b
Your Name <you@example.com>
2026-01-16 11:17:00 +0400

    Luka

--- modals/authModal.js
+++ modals/authModal.js
@@ -10,7 +10,15 @@ export function renderAuthModal() {
           <p class="modal__message" data-modal-message></p>
           <div class="auth-modal__actions">
             <button class="btn auth-btn auth-btn--google" type="button" data-auth-provider="google">
-              Continue with Google
+              <span class="auth-btn__icon" aria-hidden="true">
+                <svg viewBox="0 0 18 18" role="presentation" focusable="false">
+                  <path fill="#4285F4" d="M17.64 9.2c0-.64-.06-1.26-.17-1.86H9v3.52h4.84a4.14 4.14 0 0 1-1.8 2.72v2.26h2.92c1.71-1.58 2.68-3.91 2.68-6.64z"/>
+                  <path fill="#34A853" d="M9 18c2.43 0 4.47-.8 5.96-2.16l-2.92-2.26c-.8.54-1.83.86-3.04.86-2.35 0-4.34-1.58-5.05-3.7H.93v2.33A8.99 8.99 0 0 0 9 18z"/>
+                  <path fill="#FBBC05" d="M3.95 10.74a5.4 5.4 0 0 1 0-3.48V4.93H.93a9 9 0 0 0 0 8.14l3.02-2.33z"/>
+                  <path fill="#EA4335" d="M9 3.58c1.32 0 2.5.45 3.43 1.34l2.57-2.57C13.47.89 11.43 0 9 0 5.48 0 2.44 2.02.93 4.93l3.02 2.33C4.66 5.14 6.65 3.58 9 3.58z"/>
+                </svg>
+              </span>
+              <span>Continue with Google</span>
             </button>
             <button class="btn btn-secondary auth-btn auth-btn--create" type="button" data-open-modal="createAccountModal">
               Create account
